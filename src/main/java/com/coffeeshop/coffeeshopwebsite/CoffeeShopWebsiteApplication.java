c1ec7ac886dac3b90264efc1591ca309d562a302
Alla-Pliskovskaya <alla.pliskowskaya@yandex.ru>
2022-10-10 14:26:22 +0500

    Test commit

---
+++ src/main/java/com/coffeeshop/coffeeshopwebsite/CoffeeShopWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.coffeeshopwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeShopWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coffeeshop/coffeeshopwebsite/CoffeeShopWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.coffeeshopwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
